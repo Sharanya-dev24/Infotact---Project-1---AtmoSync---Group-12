38e1eb70d30ab6dabac6b4e6514f4d76b48bbe42
PadidalaHimabindu <159221133+PadidalaHimabindu@users.noreply.github.com>
2026-07-17 19:17:25 +0530

    Update consumer.py

---
+++ consumer.py
@@ -0,0 +1,15 @@
+import json
+from kafka import KafkaConsumer
+
+# Connect to Kafka
+consumer = KafkaConsumer(
+    "container_telemetry",
+    bootstrap_servers="localhost:9092",
+    auto_offset_reset="earliest",
+    value_deserializer=lambda x: json.loads(x.decode("utf-8"))
+)
+
+print("Waiting for messages...\n")
+
+for message in consumer:
+    print(message.value)
