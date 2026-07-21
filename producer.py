78f7d63b083a659ea5e3f634901b5ede6916fd4a
PadidalaHimabindu <159221133+PadidalaHimabindu@users.noreply.github.com>
2026-07-21 20:03:13 +0530

    Update producer.py
    for live iot_simulator

--- producer.py
+++ producer.py
@@ -1,24 +1,37 @@
 import json
-import time
-import pandas as pd
 from kafka import KafkaProducer
 
-# Connect to Kafka
+# Kafka Configuration
+KAFKA_TOPIC = "container-telemetry"
+BOOTSTRAP_SERVER = "localhost:9092"
+
+# Create Kafka Producer
 producer = KafkaProducer(
-    bootstrap_servers="localhost:9092",
+    bootstrap_servers=BOOTSTRAP_SERVER,
     value_serializer=lambda x: json.dumps(x).encode("utf-8")
 )
 
-# Read the CSV file
-# df = pd.read_csv("container_telemetry.csv")   # Change the filename if needed
-df = pd.read_csv("data/container_telemetry.csv")
-# Send each row to Kafka
-for _, row in df.iterrows():
-    producer.send("container_telemetry", row.to_dict())
-    print(f"Sent: {row['container_id']}")
-    time.sleep(1)   # Send one record every second
+print("Kafka Producer Connected!")
 
-producer.flush()
-producer.close()
+def send_to_kafka(telemetry):
+    try:
+        producer.send(KAFKA_TOPIC, value=telemetry)
+        producer.flush()
 
-print("Finished sending all records.")
+        print(
+            f"Sent -> "
+            f"Shipment: {telemetry['shipment_id']} | "
+            f"Container: {telemetry['container_id']} | "
+            f"Temperature: {telemetry['temperature']}°C | "
+            f"Humidity: {telemetry['humidity']}% | "
+            f"Alert: {telemetry['alert_level']}"
+        )
+
+    except Exception as e:
+        print(f"Error sending message: {e}")
+
+# Close Kafka Producer
+def close_producer():
+    producer.flush()
+    producer.close()
+    print("Kafka Producer Closed.")
